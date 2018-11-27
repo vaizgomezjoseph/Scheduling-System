ce8b8e3088599d4ef9ebecb9b85b225f4618b089
Hakxon <tonyle148@gmail.com>
2018-11-26 18:44:55 -0800

    Add files via upload

---
+++ javaUser/src/MyConnection.java
@@ -0,0 +1,21 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class MyConnection {
+    
+    public static Connection getConnection()
+    {
+        Connection con = null;
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/userProjDB?verifyServerCertificate=false&useSSL=true", "root", "12345678");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return con;
+    }
+}
